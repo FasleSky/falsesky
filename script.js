0f97c9d504f64d52d97bccb754faab1435256db2
FasleSky <akbit7958@gmail.com>
2026-01-01 02:58:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ let bg=new Image(); bg.src=bgSrc;
 let birdHitbox = 18;   // collision box (unchanged small size)
 let birdDisplay = 125;  // visual size (increase or tune later)
 let birdX=100,birdY=250,vel=0,gravity=0.35,lift=-5.1;
-let liftMobile = -8;   // more powerful tap jump
+let liftMobile = -9;   // more powerful tap jump
 let pipes=[],score=0,run=false,bird=0;
 
 /* --- UI BUTTONS --- */
@@ -147,6 +147,7 @@ function end(){
     run=false;
     over.style.display="block";
 }
+
 
 
 
